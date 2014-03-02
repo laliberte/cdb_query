9f2296aac71db11420c95cdf2c9dfe28f3e18a1e
laliberte <laliberte.frederic@gmail.com>
2014-03-02 16:46:54 -0500

    apply now copies the header

--- lib/netcdf_utils.py
+++ lib/netcdf_utils.py
@@ -260,9 +260,9 @@ def apply(options,project_drs):
     vars_list=[[ var[drs_to_eliminate.index(field)] if field in drs_to_eliminate else None
                         for field in database.drs.official_drs_no_version] for var in 
                         database.list_fields_local(options,drs_to_eliminate) ]
-    database.record_header(options,output_root)
 
     output_root=distributed_apply(apply_to_variable,database,options,vars_list)
+    database.record_header(options,output_root)
     output_root.close()
     return
 
