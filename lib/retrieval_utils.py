c595c7aae5777c4b10c34d59d947d4235d5b8b78
laliberte <laliberte.frederic@gmail.com>
2014-03-02 23:21:02 -0500

    another try

--- lib/retrieval_utils.py
+++ lib/retrieval_utils.py
@@ -97,7 +97,9 @@ def md5_for_file(f, block_size=2**20):
         md5.update(data)
     return md5.hexdigest()
 
-def retrieve_path_wrapper(
+#def retrieve_path_wrapper(in_tuple,pointer_var):
+#    path=in_tuple[0]
+#    return path, pointer_var
 
 def retrieve_path(path,options):
     decomposition=path[0].split('|')
@@ -156,6 +158,7 @@ def find_local_file(source_dir,data):
     return new_paths_list, new_file_type_list
 
 def retrieve_path_data(in_tuple,pointer_var):
+    print in_tuple, pointer_var
     path=in_tuple[0].replace('fileServer','dodsC').split('|')[0]
     var=in_tuple[1]
     indices=in_tuple[2]
