b0e994a55d12ef116550679ff6f3716e4b10b86f
Frederic Laliberte <laliberte.frederic@gmail.com>
2016-07-02 11:23:01 -0400

    Moved to 1.9.7.3

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ from setuptools import setup, find_packages
 package_name='cdb_query'
 setup(
        name = package_name,
-       version = "1.9.7.2",
+       version = "1.9.7.3",
        packages=[package_name],
        package_dir = {package_name: 'lib'},
 #
