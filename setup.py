be11e76b79b376414eeb19a8414897cd29d7e5be
laliberte <laliberte.frederic@gmail.com>
2016-10-07 09:07:33 -0400

    Bumped to 1.9.94

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ from setuptools import setup, find_packages
 package_name='cdb_query'
 setup(
        name = package_name,
-       version = "1.9.9.3",
+       version = "1.9.9.4",
        packages=[package_name],
        package_dir = {package_name: 'lib'},
 #
