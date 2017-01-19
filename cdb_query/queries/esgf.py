13507079a7376864b5e9767b108d44db80b0d7f8
laliberte <laliberte.frederic@gmail.com>
2017-01-19 16:20:54 -0500

    Added pyesgf

--- cdb_query/queries/esgf.py
+++ cdb_query/queries/esgf.py
@@ -14,7 +14,7 @@ import logging
 import netcdf4_soft_links.remote_netcdf.remote_netcdf as remote_netcdf
 
 # Internal:
-from .pyesgf_connection import SearchConnection
+from .esgf-pyclient import SearchConnection
 
 unique_file_id_list = ['checksum_type', 'checksum', 'tracking_id']
 
